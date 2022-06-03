2fc5a12a4c7e05f212046138316b06b36c178df8
AnaM@C <anamacam.am@gmail.com>
2022-06-03 09:34:50 -0500

    corrijo error de import main

--- src/ada.org/Main.java
+++ src/ada.org/Main.java
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 package ada.org;
 
 import ada.org.dominio.Item;
-=======
-import org.ada.dominio.Item;
->>>>>>> feature/operacion-multiplicar
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -12,19 +8,6 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
-        // byte edad =
-        //Persona ana = new Persona("Ana","Castro",edad, 1.50,'F')
-        //ana.decirNombreCompleto();
-        //double peso = 45
-        //double estatura = 45
-        //byte vida= 6
-        //Perro dogoBurdeos = new Perro("Dogo de Burdeos", "tranquilo",peso,estatura, vida,"media", "delicado");
-        //dogoBurdeos.perroRaza()
-
-        //Animal animal = new Animal("gato", "pardo","garfield" );
-        //animal.sleep();
-        //animal.eat();
-        //animal.move()
 
         Date expirationDate = new SimpleDateFormat("dd-nn-yyyy, hh:mn:ss").parse("17-05-2022 18:00:00");
         Item orangeJuice = new Item("Orange Juice", 120, expirationDate);
